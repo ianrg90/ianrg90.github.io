4e5baacc7868e2698c24ebbd044b6a334cd94d91
ianrg90 <iangladulich@gmail.com>
2021-11-24 10:18:07 -0300

    Fixing bugs

--- Projects/Genius-game/script.js
+++ Projects/Genius-game/script.js
@@ -9,19 +9,21 @@ let roundColors = [];
 let clickedColors = [];
 let roundNumber = 1;
 let counter = 0;
-let isRoundDone = false
+let isRoundDone = false;
+let isGameRunning = false;
 
 function gameRound() {
+  isGameRunning = true;
   counter++;
   round.textContent = roundNumber;
   if (counter > roundNumber) {
-    isRoundDone = true
+    isRoundDone = true;
     return;
   } else {
     const randomColor = Math.floor(Math.random() * 4);
     roundColors.push(randomColor);
     colors[randomColor].classList.add("selected");
-    timer = setTimeout(() => {
+    const timer = setTimeout(() => {
       colors[randomColor].classList.remove("selected");
     }, 500);
 
@@ -30,49 +32,62 @@ function gameRound() {
 }
 
 function getCickedColor() {
-  if(!isRoundDone){
-    return
+  if (!isRoundDone) {
+    alert("Wait the end of the round to start clicking colors")
+    return;
   }
-  if(clickedColors.length < roundColors.length){
-    clickedColors.push(+this.dataset.number)
+  if (clickedColors.length < roundColors.length) {
+    clickedColors.push(+this.dataset.number);
   }
-  if(clickedColors.length === roundColors.length && checkAnswer(roundColors,clickedColors)){
-    roundColors.splice(0,roundColors.length)
-    clickedColors.splice(0,clickedColors.length)
-    counter = 0
-    roundNumber++
-    alert("You did it! ready for next round ?")
-    isRoundDone = false
-    setTimeout(gameRound, 500)
+  if (
+    clickedColors.length === roundColors.length &&
+    checkAnswer(roundColors, clickedColors)
+  ) {
+    roundColors.splice(0, roundColors.length);
+    clickedColors.splice(0, clickedColors.length);
+    counter = 0;
+    roundNumber++;
+    alert("You did it! ready for next round ?");
+    isRoundDone = false;
+    setTimeout(gameRound, 500);
   }
-  if(clickedColors.length === roundColors.length && !checkAnswer(roundColors, clickedColors)){
-    alert(`You lost in round ${roundNumber}`)
-    restart()
+  if (
+    clickedColors.length === roundColors.length &&
+    !checkAnswer(roundColors, clickedColors)
+  ) {
+    alert(`You lost in round ${roundNumber}`);
+    restart();
   }
 }
 
 //Helper function to check if arrays are equal
-function checkAnswer(arr1, arr2){
+function checkAnswer(arr1, arr2) {
   const check = arr1.map((item, i) => {
-    return item === arr2[i]
-  })
-  return check.every(item => item === true)
+    return item === arr2[i];
+  });
+  return check.every((item) => item === true);
 }
 
-function restart(){
-  counter = 0
-  roundNumber = 1
-  roundColors.splice(0,roundColors.length)
-  clickedColors.splice(0,clickedColors.length)
-  round.textContent = 0
-  isRoundDone = false
-
+function restart() {
+  counter = 0;
+  roundNumber = 1;
+  roundColors.splice(0, roundColors.length);
+  clickedColors.splice(0, clickedColors.length);
+  round.textContent = 0;
+  isRoundDone = false;
+  isGameRunning = false;
 }
 
 function startGame() {
-  gameRound()
+  if (isGameRunning) {
+    return;
+  }
+  gameRound();
 }
 
 start.addEventListener("click", startGame);
-restartButton.addEventListener("click", restart)
+restartButton.addEventListener("click", restart);
 colors.forEach((color) => color.addEventListener("click", getCickedColor));
+
+
+
